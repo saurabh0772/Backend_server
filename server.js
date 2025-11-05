190032ea29807a0f1167c6ac53ee155d119e8271
Saurabh Kumar <143883080+saurabh0772@users.noreply.github.com>
2025-11-05 09:37:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,13 +21,14 @@ app.get("/api/simple", (req, res) => {
 
 app.get("/api/fees", (req, res) => {
     res.json({
-        BTech : "1.5L to 2L per year",
-        BCA: "1L to 1.5L per year",
-        MCA : "1.5L to 2L per year",
-        MTech : "2L to 2.5L per year"
+        BTech : {range : "1.5L to 2L per year"},
+        BCA: {range : "1L to 1.5L per year"},
+        MCA : {range : "1.5L to 2L per year"},
+        MTech : {range : "2L to 2.5L per year"}
     });
 })
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}`));
+
 
